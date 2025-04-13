36c7aac5c0426a67ac93f08e097bad587623da90
Zikryyy <zkry098zkry@gmail.com>
2025-04-13 12:43:59 +0800

    thirdcommit

--- frontend/frontend.py
+++ frontend/frontend.py
@@ -101,7 +101,7 @@ if st.button("🚀 Predict Maturity"):
         "humidity": hum
     }
     try:
-        response = requests.post("http://127.0.0.1:5000/predict", json=data)
+        response = requests.post("https://maize-frontend.onrender.com", json=data)
         result = response.json()
         if "prediction" in result:
             prediction = result["prediction"]
